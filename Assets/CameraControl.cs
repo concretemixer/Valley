b9b3638c9ea50ba303c6794cd88af4dff9d42cb6
max <max.pervukhin@gmail.com>
2017-06-20 02:55:21 +0700

    2

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            transform.position += Vector3.up;
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            transform.position += Vector3.down;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            transform.position += Vector3.left;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            transform.position += Vector3.right;
+
+
+    }
+}
